5a0f32568f386c40c52fc47947d5f658df44f43a
rayhan randi <rayhanrandi10@gmail.com>
2025-05-08 22:56:00 +0700

    fix: allow publish to fail

--- companion/publish.py
+++ companion/publish.py
@@ -15,38 +15,33 @@ logger = Logger().setup_logger(service_name=__name__)
 
 
 def publish_message(data, queue):
-    try:
-        data_bytes = data.encode('utf-8')
-        credentials = pika.PlainCredentials(user, password)
-        publish_connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=rabbit_ip,
-                credentials=credentials,
-                virtual_host=application
-            )
+    data_bytes = data.encode('utf-8')
+    credentials = pika.PlainCredentials(user, password)
+    publish_connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=rabbit_ip,
+            credentials=credentials,
+            virtual_host=application
         )
-        
-        channel = publish_connection.channel()
-        status = channel.queue_declare(
-            queue=queue, 
-            durable=True,
-            arguments={ "x-queue-type": "quorum" }
-        )
-        
-        if status.method.message_count == 0:
-            logger.info("queue empty")
+    )
+    
+    channel = publish_connection.channel()
+    status = channel.queue_declare(
+        queue=queue, 
+        durable=True,
+    )
+    
+    if status.method.message_count == 0:
+        logger.info("queue empty")
 
-        channel.basic_publish(
-            exchange='',
-            routing_key=queue,
-            body=data_bytes,
-            properties=pika.BasicProperties(
-                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
-            )
+    channel.basic_publish(
+        exchange='',
+        routing_key=queue,
+        body=data_bytes,
+        properties=pika.BasicProperties(
+            delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
         )
-        logger.info("message delivered", flush=True)
-        
-        publish_connection.close()
-    except Exception as e:
-        logger.warn('failed to send message.')
-        logger.error(repr(e))
+    )
+    logger.info("message delivered")
+    
+    publish_connection.close()
