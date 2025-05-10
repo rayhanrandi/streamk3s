f21b6860f306ac58d8ed216813ed03c447e6a082
rayhan randi <rayhanrandi10@gmail.com>
2025-05-10 16:10:32 +0700

    feat: impl base retry logic

--- companion/Interface.py
+++ companion/Interface.py
@@ -1,41 +1,104 @@
-import os
 import json
+import os
+
+from multiprocessing import Process, Queue
+
 import consume
-import flask
-from flask import Flask, jsonify
 import publish
+
+import flask
+import pika
+
+from flask import Flask, jsonify
 
 from config.logging import logger
 
+
+QUEUE_MAXSIZE: int = 10000
+
 app = Flask(__name__)
 
+deadletter_queue: Queue = Queue(maxsize=QUEUE_MAXSIZE)
+
+queue = os.getenv("OUTPUT_QUEUE", "#queue")
 termination = os.getenv("TERMINATION_QUEUE", "#termination")
 
 
+# class MessageQueue:
+
+#     _APPLICATION = os.getenv("APPLICATION", "#APPLICATION")
+#     _RABBIT_IP = os.getenv("RABBIT_IP", "10.100.59.176")
+#     _USER = 'user'
+#     _PASSWORD = os.getenv("RABBITMQ_PASSWORD", "o1mB8moVLo")
+
+#     def __init__(self) -> None:
+#         """
+#         Initializes a BlockingConnection to RabbitMQ.
+#         """
+#         logger.info('initializing BlockingConnection and BlockingChannel...')
+#         try:
+#             self._credentials = pika.PlainCredentials(self._USER, self._PASSWORD)
+#             self._conn_params = pika.ConnectionParameters(
+#                 host=self._RABBIT_IP,
+#                 credentials=self._credentials,
+#                 virtual_host=self._APPLICATION
+#             )
+#             self._conn = pika.BlockingConnection(self._conn_params)
+#             self._is_connected = Value('i', 1)
+#         except Exception as e:
+#             logger.error(f'error while initializing connection: {str(e)}')
+
+#     @retry(wait=wait_exponential(multiplier=1, min=0, max=3))
+#     def reconnect(self) -> None:
+#         try:
+#             self._conn.close()
+#         except:
+#             pass
+#         self._conn = pika.BlockingConnection(self._conn_params)
+
+#     def get_connection(self) -> pika.BlockingConnection:
+#         return self._conn
+    
+
 @app.route("/post_message", methods=["POST"])
-def post():
+async def post():
     json_string = flask.request.data.decode("utf-8")
 
     try:
         json_data = json.loads(json_string)
     except json.JSONDecodeError as e:
+        logger.error(f"Failed while decoding JSON data: {e.msg}")
         return jsonify({"error": "Invalid JSON data"}), 400
 
-    queue = os.getenv("OUTPUT_QUEUE", "#queue")
+    target_queue = queue
     if json_data.get("status") and termination != "#termination":
-        queue = termination
+        logger.info("Termination message received.")
+        target_queue = termination
 
-    logger.info(json_string)
-    publish.publish_message(json_string, queue)
+    # publish message in child processes
+    logger.info(f'spawning child process to publish message: f{json_string}...')
+    p_publisher = Process(
+        target=publish.with_retry_publish,
+        args=(json_string, target_queue, deadletter_queue,)
+    )
+    p_publisher.start()
 
+    logger.info(f'DLQ size: {deadletter_queue.qsize()}')
+    
     return jsonify(json_data)
 
 
 @app.route("/get_message", methods=["GET"])
-def get():
+async def get():
     rabbit_input_queue = os.getenv("INPUT_QUEUE", "queue-1")
     json_data = consume.consume_message(rabbit_input_queue)
     return json_data
 
 
-app.run(host="0.0.0.0", port=4321)
+if __name__ == '__main__':
+    p_dlq = Process(
+        target=publish.publish_failover,
+        args=(deadletter_queue, queue,)
+    )
+    p_dlq.start()
+    app.run(host="0.0.0.0", port=4321)
--- companion/publish.py
+++ companion/publish.py
@@ -1,31 +1,61 @@
 import os
+from multiprocessing import Queue
+
 import pika
+
+from tenacity import (
+    retry, 
+    stop_after_attempt, stop_after_delay, wait_exponential,
+)
 
 from config.logging import logger
 
 
-message_list = []
-
-rabbit_ip = os.getenv("RABBIT_IP", "10.100.59.176")
-user = 'user'
-password = os.getenv("RABBITMQ_PASSWORD", "o1mB8moVLo")
-application = os.getenv("APPLICATION", "#application")
+RABBIT_IP = os.getenv("RABBIT_IP", "10.100.59.176")
+USER = 'user'
+PASSWORD = os.getenv("RABBITMQ_PASSWORD", "o1mB8moVLo")
+APPLICATION = os.getenv("APPLICATION", "#APPLICATION")
 
 
-def publish_message(data, queue):
+def with_retry_publish(data: str, target_queue: str, deadletter_queue: Queue) -> pika.BlockingConnection:
+    try:
+        publish_message(data, target_queue)
+    except:
+        logger.warn('failed to publish message. triggering failover...')
+        deadletter_queue.put(data)
+        logger.info('message put to failover queue')
+
+
+def publish_failover(deadletter_queue: Queue, target_queue: str) -> None:
+    while True:
+        if not deadletter_queue.empty():
+            data = deadletter_queue.get()
+            try:
+                publish_message(data, target_queue)
+            except:
+                logger.warn('failed to publish DLQ message. triggering failover...')
+                deadletter_queue.put(data)
+                logger.info('message put to failover queue')
+
+
+@retry(
+        wait=wait_exponential(multiplier=1, min=0, max=10),
+        stop=(stop_after_attempt(5) | stop_after_delay(15)),
+)
+def publish_message(data: str, target_queue: str):
     data_bytes = data.encode('utf-8')
-    credentials = pika.PlainCredentials(user, password)
+    credentials = pika.PlainCredentials(USER, PASSWORD)
     publish_connection = pika.BlockingConnection(
         pika.ConnectionParameters(
-            host=rabbit_ip,
+            host=RABBIT_IP,
             credentials=credentials,
-            virtual_host=application
+            virtual_host=APPLICATION
         )
     )
     
     channel = publish_connection.channel()
     status = channel.queue_declare(
-        queue=queue, 
+        queue=target_queue, 
         durable=True,
     )
     
@@ -34,7 +64,7 @@ def publish_message(data, queue):
 
     channel.basic_publish(
         exchange='',
-        routing_key=queue,
+        routing_key=target_queue,
         body=data_bytes,
         properties=pika.BasicProperties(
             delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
@@ -43,3 +73,4 @@ def publish_message(data, queue):
     logger.info("message delivered")
     
     publish_connection.close()
+    
